0a587965f18b45983e44cdcc850ebb8139e03e69
eldiegomoustachu <31035533+eldiegomoustachu@users.noreply.github.com>
2018-09-25 19:52:18 +0200

    Fixed a bug with "ghosty" command

--- app.js
+++ app.js
@@ -181,7 +181,7 @@ bot.on('message', (msg) => {
 
     // Ghosty
     if(msg.content == "ghosty"){
-        msg.react('<:ghosty:478615972216045568>');
+        msg.react('478615972216045568');
     }
     // Ghost
     if(msg.content.startsWith("")) {
